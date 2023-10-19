1962d51ab69b57ef893b2b6aaa072d76c8c2dc19
Sanjana <sanjana.js02@gmail.com>
2023-10-19 12:23:16 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require('./models/Recipe.model');
 
 const app = express();
 
@@ -11,7 +13,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! express-mongoose-recipes-dev: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -23,23 +30,69 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async (req, res) => {
+    try {
+      const newRecipe = await Recipe.create(req.body);
+      res.status(201).json({ recipe: newRecipe });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
-
+app.get("/recipes", async (req, res) => {
+    try {
+      const recipes = await Recipe.find();
+      res.status(200).json({ recipes });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
-
+app.get("/recipes/:recipeId", async (req, res) => {
+    const { recipeId } = req.params;
+    try {
+      const recipe = await Recipe.findById(recipeId);
+      if (!recipe) {
+        res.status(404).json({ message: "Recipe not found" });
+      } else {
+        res.status(200).json({ recipe });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
+  
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
-
+app.put("/recipes/:recipeId", async (req, res) => {
+    const { recipeId } = req.params;
+    try {
+      const updatedRecipe = await Recipe.findByIdAndUpdate(recipeId, req.body, { new: true });
+      if (!updatedRecipe) {
+        res.status(404).json({ message: "Recipe not found" });
+      } else {
+        res.status(200).json({ recipe: updatedRecipe });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete('/recipes/:recipesId', async (request, response) => {
+    const { recipesId } = request.params
+  
+    await Recipe.findByIdAndDelete(recipesId)
+    response.status(202).json({ message: 'Recipe deleted' })
+  })
 
 // BONUS
 //  Bonus: Iteration 9 - Create a Single User
